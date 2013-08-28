0b2b5b1cc82a57ffc4bd7ba210a6fb46ae6f3228
Achilleas <achilleas@tessa>
2013-08-28 16:33:10 +0300

    Trailing whitespace

--- brian2/core/network.py
+++ brian2/core/network.py
@@ -6,7 +6,7 @@ from brian2.core.names import Nameable
 from brian2.core.base import BrianObject
 from brian2.core.clocks import Clock
 from brian2.units.fundamentalunits import check_units
-from brian2.units.allunits import second 
+from brian2.units.allunits import second
 from brian2.core.preferences import brian_prefs
 from brian2.core.namespace import get_local_namespace
 
@@ -18,7 +18,7 @@ logger = get_logger(__name__)
 class Network(Nameable):
     '''
     Network(*objs, name='network*')
-    
+
     The main simulation controller in Brian
 
     `Network` handles the running of a simulation. It contains a set of Brian
@@ -27,7 +27,7 @@ class Network(Nameable):
     objects get called in what order is described in detail in the notes below.
     The objects in the `Network` are accesible via their names, e.g.
     `net['neurongroup']` would return the `NeuronGroup` with this name.
-    
+
     Parameters
     ----------
     objs : (`BrianObject`, container), optional
@@ -43,9 +43,9 @@ class Network(Nameable):
 
     Notes
     -----
-    
+
     The main run loop performs the following steps:
-    
+
     1. Prepare the objects if necessary, see `~Network.prepare`.
     2. Determine the end time of the simulation as `~Network.t`+``duration``.
     3. Determine which set of clocks to update. This will be the clock with the
@@ -61,8 +61,8 @@ class Network(Nameable):
        not be called if the `~BrianObject.active` flag is set to ``False``.
        The order in which the objects are called is described below.
     6. Increase `Clock.t` by `Clock.dt` for each of the clocks and return to
-       step 2. 
-    
+       step 2.
+
     The order in which the objects are updated in step 4 is determined by
     the `Network.schedule` and the objects `~BrianObject.when` and
     `~BrianObject.order` attributes. The `~Network.schedule` is a list of
@@ -74,10 +74,10 @@ class Network(Nameable):
     those with ``when=='groups'``, and so forth. If several objects have the
     same `~BrianObject.when` attribute, then the order is determined by the
     `~BrianObject.order` attribute (lower first).
-    
+
     See Also
     --------
-    
+
     MagicNetwork, run, stop
     '''
 
@@ -88,7 +88,7 @@ class Network(Nameable):
         #: Stores references or `weakref.proxy` references to the objects
         #: (depending on `weak_references`)
         self.objects = []
-        
+
         name = kwds.pop('name', 'network*')
 
         #: Whether the network only stores weak references to the objects
@@ -101,10 +101,10 @@ class Network(Nameable):
 
         for obj in objs:
             self.add(obj)
-            
+
         #: Current time as a float
-        self.t_ = 0.0   
-     
+        self.t_ = 0.0
+
     t = property(fget=lambda self: self.t_*second,
                  fset=lambda self, val: setattr(self, 't_', float(val)),
                  doc='''
@@ -144,10 +144,10 @@ class Network(Nameable):
     def add(self, *objs):
         """
         Add objects to the `Network`
-        
+
         Parameters
         ----------
-        
+
         objs : (`BrianObject`, container)
             The `BrianObject` or container of Brian objects to be added. Specify
             multiple objects, or lists (or other containers) of objects.
@@ -172,10 +172,10 @@ class Network(Nameable):
     def remove(self, *objs):
         '''
         Remove an object or sequence of objects from a `Network`.
-        
+
         Parameters
         ----------
-        
+
         objs : (`BrianObject`, container)
             The `BrianObject` or container of Brian objects to be removed. Specify
             multiple objects, or lists (or other containers) of objects.
@@ -202,12 +202,12 @@ class Network(Nameable):
     def reinit(self):
         '''
         Reinitialises all contained objects.
-        
+
         Calls `BrianObject.reinit` on each object.
         '''
         for obj in self.objects:
             obj.reinit()
-    
+
     def _get_schedule(self):
         if not hasattr(self, '_schedule'):
             self._schedule = ['start',
@@ -217,50 +217,50 @@ class Network(Nameable):
                               'resets',
                               'end',
                               ]
-        return self._schedule            
-    
+        return self._schedule
+
     def _set_schedule(self, schedule):
         self._schedule = schedule
         logger.debug("Set network {self.name} schedule to "
                      "{self._schedule}".format(self=self),
                      "_set_schedule")
-    
+
     schedule = property(fget=_get_schedule,
                         fset=_set_schedule,
                         doc='''
         List of ``when`` slots in the order they will be updated, can be modified.
-        
+
         See notes on scheduling in `Network`. Note that additional ``when``
         slots can be added, but the schedule should contain at least all of the
         names in the default schedule:
         ``['start', 'groups', 'thresholds', 'synapses', 'resets', 'end']``.
         ''')
-    
+
     def _sort_objects(self):
         '''
         Sorts the objects in the order defined by the schedule.
-        
+
         Objects are sorted first by their ``when`` attribute, and secondly
         by the ``order`` attribute. The order of the ``when`` attribute is
         defined by the ``schedule``.
         '''
         when_to_int = dict((when, i) for i, when in enumerate(self.schedule))
         self.objects.sort(key=lambda obj: (when_to_int[obj.when], obj.order))
-    
+
     def pre_run(self, namespace):
         '''
         Prepares the `Network` for a run.
-        
+
         Objects in the `Network` are sorted into the correct running order, and
         their `BrianObject.pre_run` methods are called.
-        '''                
+        '''
         brian_prefs.check_all_validated()
-        
+
         self._clocks = set(obj.clock for obj in self.objects)
-        
+
         self._stopped = False
         Network._globally_stopped = False
-        
+
         self._sort_objects()
 
         logger.debug("Preparing network {self.name} with {numobj} "
@@ -268,7 +268,7 @@ class Network(Nameable):
                         numobj=len(self.objects),
                         objnames=', '.join(obj.name for obj in self.objects)),
                      "pre_run")
-        
+
         for obj in self.objects:
             obj.pre_run(namespace)
 
@@ -277,29 +277,29 @@ class Network(Nameable):
                         num=len(self._clocks),
                         clocknames=', '.join(obj.name for obj in self._clocks)),
                      "pre_run")
-    
+
     def post_run(self):
         for obj in self.objects:
-            obj.post_run()        
-        
+            obj.post_run()
+
     def _nextclocks(self):
         minclock = min(self._clocks, key=lambda c: c.t_)
         curclocks = set(clock for clock in self._clocks if
                         (clock.t_ == minclock.t_ or
                          abs(clock.t_ - minclock.t_)<Clock.epsilon))
         return minclock, curclocks
-    
+
     @check_units(duration=second, report_period=second)
     def run(self, duration, report=None, report_period=60*second,
             namespace=None, level=0):
         '''
         run(duration, report=None, report_period=60*second)
-        
+
         Runs the simulation for the given duration.
-        
+
         Parameters
         ----------
-        
+
         duration : `Quantity`
             The amount of simulation time to run for.
         report : {None, 'stdout', 'stderr', 'graphical', function}, optional
@@ -315,7 +315,7 @@ class Network(Nameable):
         namespace : dict-like, optional
             A namespace in which objects which do not define their own
             namespace will be run. If not namespace is given, the locals and
-            globals around the run function will be used. 
+            globals around the run function will be used.
         level : int, optional
             How deep to go down the stack frame to look for the locals/global
             (see `namespace` argument). Only used by run functions that call
@@ -323,11 +323,11 @@ class Network(Nameable):
             additional nesting.
         Notes
         -----
-        
+
         The simulation can be stopped by calling `Network.stop` or the
         global `stop` function.
         '''
-        
+
         if namespace is not None:
             self.pre_run(('explicit-run-namespace', namespace))
         else:
@@ -340,9 +340,9 @@ class Network(Nameable):
         t_end = self.t+duration
         for clock in self._clocks:
             clock.set_interval(self.t, t_end)
-            
+
         # TODO: progress reporting stuff
-        
+
         # Find the first clock to be updated (see note below)
         clock, curclocks = self._nextclocks()
         if report is not None:
@@ -368,16 +368,16 @@ class Network(Nameable):
                 c.tick()
             # find the next clocks to be updated. The < operator for Clock
             # determines that the first clock to be updated should be the one
-            # with the smallest t value, unless there are several with the 
+            # with the smallest t value, unless there are several with the
             # same t value in which case we update all of them
             clock, curclocks = self._nextclocks()
-            
+
         self.t = t_end
 
         if report is not None:
             print 'Took ', current-start, 's in total.'
         self.post_run()
-        
+
     def stop(self):
         '''
         Stops the network from running, this is reset the next time `Network.run` is called.
