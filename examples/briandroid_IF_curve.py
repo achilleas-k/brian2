409b5feb7ac1242ff74ab1569c374aed5514b120
Achilleas <achilleas@tessa>
2013-09-07 20:27:46 +0300

    Basic Android codegen working - no special functions

---
+++ examples/briandroid_IF_curve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+'''
+Input-Frequency curve of a IF model
+Network: 1000 unconnected integrate-and-fire neurons (leaky IF)
+with an input parameter v0.
+The input is set differently for each neuron.
+'''
+from pylab import *
+from brian2 import *
+from brian2.codegen.languages import *
+
+N = 1000
+tau = 10 * ms
+eqs = '''
+dv/dt=(v0-v)/tau : volt (unless-refractory)
+v0 : volt
+'''
+group = NeuronGroup(N, model=eqs, threshold='v>10 * mV',
+                    reset='v = 0 * mV', refractory=5*ms,
+                    name="lifnrngrp",
+                    codeobj_class=JavaCodeObject)
+group.v = 0 * mV
+group.v0 = linspace(0 * mV, 20 * mV, N)
+mynetwork = Network(group, name="Test")
+monitor = SpikeMonitor(group)
+mynetwork.generate_code()
+print("Code generation complete!")
+
