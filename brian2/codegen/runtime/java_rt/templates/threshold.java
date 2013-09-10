5e19553016caba1b58ca891bd0b886b242555984
Achilleas <achilleas@tessa>
2013-09-10 21:25:45 +0300

    Template updating - most templates are out of date

--- brian2/codegen/runtime/java_rt/templates/threshold.java
+++ brian2/codegen/runtime/java_rt/templates/threshold.java
@@ -2,38 +2,34 @@
 //// MAIN CODE /////////////////////////////////////////////////////////////
 
 {% macro main() %}
-	// USE_SPECIFIERS { _num_neurons, not_refractory, lastspike, t }
-	////// SUPPORT CODE ///////
-	{% for line in support_code_lines %}
-	// {{line}}
-	{% endfor %}
+    // USE_SPECIFIERS { _num_neurons, not_refractory, lastspike, t }
+    ////// SUPPORT CODE ///////
+    {% for line in support_code_lines %}
+    // {{line}}
+    {% endfor %}
 
-	//// MAIN CODE ////////////
-	ArrayList<Integer> _spikes_space = new ArrayList<Integer>();
-        // TODO: use normal array so I can pass to renderscript and add an extra variable for the number of spikes
-        // array size would be number of neurons
-	for(int _neuron_idx=0; _neuron_idx<_num_neurons; _neuron_idx++)
-	{
-	    final int _vectorisation_idx = _neuron_idx;
-		{% for line in code_lines %}
-		{{line}}
-		{% endfor %}
-		if(_cond) {
-                    _spikes_space.add(_neuron_idx);
-                    not_refractory[_neuron_idx] = false;
-                    lastspike[_neuron_idx] = t;
-		}
-	}
-	return_val = _spikes_space.size();
+    //// MAIN CODE ////////////
+    ArrayList<Integer> _spikes_space = new ArrayList<Integer>();
+    // TODO: use normal array so I can pass to renderscript and add an extra variable for the number of spikes
+    // array size would be number of neurons
+    {% for line in code_lines %}
+    {{line}}
+    {% endfor %}
+    if(_cond) {
+        _spikes_space.add(_idx);
+        not_refractory[_idx] = false;
+        lastspike[_idx] = t;
+    }
+    return_val = _spikes_space[_num_neurons];
 {% endmacro %}
 
 ////////////////////////////////////////////////////////////////////////////
 //// SUPPORT CODE //////////////////////////////////////////////////////////
 
 {% macro support_code() %}
-	{% for line in support_code_lines %}
-	// {{line}}
-	{% endfor %}
+    {% for line in support_code_lines %}
+    // {{line}}
+    {% endfor %}
 {% endmacro %}
 
 
