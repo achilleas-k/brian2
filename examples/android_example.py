25cd9aee339d7b6929d281db60e582a581b083cf
Achilleas <achilleas@tessa>
2013-09-16 17:05:10 +0300

    Rename android device example

---
+++ examples/android_example.py
@@ -0,0 +1,25 @@
+from numpy import *
+from brian2 import *
+from brian2.devices.android_standalone import android_device
+
+set_device('android')
+##### Define the model
+tau_a = 10*ms
+tau_b = 20*ms
+eqs_a = '''
+dV/dt = -V/tau_a : volt (unless-refractory)
+'''
+eqs_b = '''
+dV/dt = -V/tau_b : volt (unless-refractory)
+'''
+threshold = 'V>-50*mV'
+reset = 'V=-60*mV'
+refractory = 5*ms
+N = 1000
+
+G = NeuronGroup(N, eqs_a, reset=reset, threshold=threshold, refractory=refractory, name='gp')
+G2 = NeuronGroup(10, eqs_b, reset=reset, threshold=threshold, refractory=refractory, name='gp2')
+G.V = -1*mV
+SM = SpikeMonitor(G)
+net = Network(G, G2)
+android_device.build(net)
